f0a1c39af9b21512bbe32a52749cbbd516902365
Nik <kirillovnick@mail.ru>
2022-12-29 16:22:30 +0300

    add feature/di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,43 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong id = new AtomicLong(0);
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        if (posts.isEmpty()) return Collections.emptyList();
+        return new ArrayList<>(posts.values());
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            long newId = id.incrementAndGet();
+            post = new Post(newId, post.getContent());
+            posts.put(post.getId(), post);
+        }
+        if (posts.containsKey(post.getId())) {
+            posts.put(post.getId(), post);
+        } else {
+            throw new NotFoundException();
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,55 +1,63 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
+import ru.netology.exception.NotFoundException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
+    private PostController postController;
+    private static final String PATH_POSTS = "/api/posts";
+    private static final String PATH_WITH_NUMBER_POST = PATH_POSTS + "/\\d+";
+    private static final String SLASH = "/";
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Override
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        postController = context.getBean(PostController.class);
     }
-  }
-}
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+                final var path = req.getRequestURI();
+                final var method = req.getMethod();
+                if (method.equals(GET) && path.equals(PATH_POSTS)) {
+                    postController.all(resp);
+                    return;
+                }
+                if (method.equals(GET) && path.matches(PATH_WITH_NUMBER_POST)) {
+                    // easy way
+                    final var id = Long.parseLong(path.substring(path.lastIndexOf(SLASH) + 1));
+                    postController.getById(id, resp);
+                    return;
+                }
+                if (method.equals(POST) && path.equals(PATH_POSTS)) {
+                    postController.save(req.getReader(), resp);
+                    return;
+                }
+                if (method.equals(DELETE) && path.matches(PATH_WITH_NUMBER_POST)) {
+                    // easy way
+                    final long id = Long.parseLong(path.substring(path.lastIndexOf(SLASH) + 1));
+                    postController.removeById(id, resp);
+                    return;
+                }
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            } catch (IOException ex) {
+            ex.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        catch (NotFoundException ex) {
+            ex.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+}
